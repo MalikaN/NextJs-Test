6bdbd5020338d9d14983d7ac11149e80043005fd
MalikaN <indee89@gmail.com>
2018-03-13 13:44:47 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Header from '../comps/Header'
+import Link from 'next/link'
+import Layout from '../comps/MyLayout'
+
+const About = () =>(
+    <Layout>
+        <p>About US page</p>
+    </Layout>
+)
+
+export default About
